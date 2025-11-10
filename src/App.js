22255540018c98347173deeb755061fd9765b9b8
arvindsilswal009-svg <arvindsilswal009@gmail.com>
2025-11-10 14:33:50 +0530

    lets go

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const [city,setcity]=useState();
+const [weather,setweather]=useState();
+  const handlecitychange = (e)=>{
+    setcity(e.target.value);
+
+  }
+  const fetchweather = async()=>{
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'465b6b9fa0eddf836ea5aa487594cf6d'}`);
+      setweather(response);
+      document.getElementsByClassName('result')[0].style.display='block';
+    } catch (error) {
+      console.log("error fetching data",error)
+    }}
+  const handleclick =()=>{
+  fetchweather();
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+<div className='weather'> 
+  <center><a><i class="fa-solid fa-cloud-sun-rain" ></i></a></center>
+  <center><label ><h4>Weather Forcast</h4></label></center>
+  
+<div>
+  
+<input className='form-control' type='text' value={city} onChange={handlecitychange} placeholder='Enter City Name'/>
+<br/>
+<center><button  type='submit' onClick={handleclick}>Get Weather</button></center>
+</div>
+</div>
+ 
+<div className='result' style={{display:'none'}}>
+
+  {weather &&(
+    <div>
+      <center><a><i class="fa-solid fa-cloud-sun-rain" ></i></a></center>
+      
+      <h4>Weather in {weather.data.name}</h4>
+      <br/>
+      <p>Temperature: {(weather.data.main.temp - 273.15).toFixed(2)} °C</p>
+      <br/>
+      <p>Humidity: {weather.data.main.humidity} %</p>
+      <br/>
+      <p>Condition: {weather.data.weather[0].description}</p>
+      <br/><br/>
+      <center><p>Note : The weather Conditions are accurate according to your city </p></center>
+      
     </div>
+  )}
+
+</div>
+    </div>
+    </>
   );
 }
 
---
+++ src/apikeys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    key:"465b6b9fa0eddf836ea5aa487594cf6d"
+};
